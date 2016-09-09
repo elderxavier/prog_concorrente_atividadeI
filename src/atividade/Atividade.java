76e08e189d75422d9bfa03a2e6f7ff9123d86ca3
Elder Xavier <elder@easysystem.com.br>
2016-09-09 01:34:01 -0300

    Finalizado

--- src/atividade/Atividade.java
+++ src/atividade/Atividade.java
@@ -7,10 +7,7 @@ import br.edu.sumare.pergunta8.Pergunta8;
 public class Atividade {
 
     public static void main(String[] args) {
-        /*Pergunta8 pergunta = new Pergunta8();
-        pergunta.Executa();*/
-        Pergunta10 pergunta = new Pergunta10();
-        pergunta.Executa();        
+       System.out.println("Execute cada uma das classes");
         
     }
 
